844512ed3539e07627de52d2194e720b2ec2ac4a
bluestar_ <canikissfemboy@gmail.com>
2024-02-03 09:22:34 +0700

    Fix header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-4 py-2">
-      <h1 className="text-lg md:text-2xl items-center font-bold text-gray-900 dark:text-white">
-        notmycode blog
+      <h1 className="text-md md:text-2xl items-center font-bold text-gray-900 dark:text-white">
+        notmycode
       </h1>
-      <nav className="flex gap-4 sm:gap-6">
+      <nav className="flex gap-4 sm:gap-6 text-sm md:text-lg">
         <Link className="text-gray-900 dark:text-white" href="/">
           Home
         </Link>
