95f77a88406dc119e2d6359cff1dc1773237073c
bluestar_ <canikissfemboy@gmail.com>
2024-02-03 09:36:24 +0700

    Added about page(WIP)

---
+++ src/app/about/page.tsx
@@ -0,0 +1,22 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <div className="p-4 rounded-lg">
+        <h1 className="text-3xl font-bold mb-4">About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor
+          erat a blandit consequat. Nulla facilisi. Donec vulputate turpis a
+          fringilla condimentum. Sed vel ultrices nunc. Nam eget porttitor
+          massa, sed condimentum nisl. Pellentesque habitant morbi tristique
+          senectus et netus et malesuada fames ac turpis egestas. Sed eget justo
+          sit amet libero pharetra iaculis. Duis nec justo et mauris fermentum
+          tincidunt. Curabitur euismod arcu ut dui ultrices, eu venenatis massa
+          condimentum. Sed sed arcu eget lectus mattis tristique at in arcu.
+          Nulla facilisi. Mauris euismod lorem id magna hendrerit, sed
+          consectetur urna volutpat. Nullam finibus, nisl non varius efficitur,
+          nisl urna laoreet quam, vitae posuere libero ipsum a mauris.
+        </p>
+      </div>
+    </main>
+  )
+}
